348b2f54b089ba7e5e1db322b4b40bda3f6259b4
Nj3 <natarajitachi@hotmail.com>
2017-05-20 21:49:16 +0530

    added 1 method

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -1,24 +1,47 @@
+import edu.princeton.cs.algs4.StdIn;
+
 //import edu.princeton.cs.algs4.StdRandom;
 //import edu.princeton.cs.algs4.StdStats;
-//import edu.princeton.cs.algs4.QuickFindUF;
+import edu.princeton.cs.algs4.QuickFindUF;
 
 public class Percolation {
-	           
-	//public    void open(int row, int col);    // open site (row, col) if it is not open already
+	
+	public static int N = 0;
+	public int[][] grid;
+	public int[][] site;
+	public int[] uf;
+	
 	//public boolean isFull(int row, int col); // is site (row, col) full?
 	//public     int numberOfOpenSites();       // number of open sites
 	//public boolean percolates();              // does the system percolate?
 
 	public Percolation(int n) {
 		// create n-by-n grid, with all sites blocked
-		int[][] grid = new int[n][n];
-		int val = 0;
+		
+		if( n <= 0 ) {
+			throw new java.lang.IllegalArgumentException();
+		}
+		
+		int[][] tmp = new int[n][n];
+
 		for(int i = 0; i < n; ++i) {
 			for(int j = 0; j < n; ++j) {
-				grid[i][j] = val;
-				++val;
+				tmp[i][j] = 1;
 			}
 		}
+		
+		// create another n-by-n grid which tells the name of the site with numbers from 0 to N*N - 1
+		int[][] temp_site = new int[n][n];
+		int name = 0;
+		for(int i = 0; i<n; ++i) {
+			for(int j = 0; j<n; ++j) {
+				temp_site[i][j] = name++;
+			}
+		}
+		grid = tmp;
+		site = temp_site;
+		
+		// ensuring all sites are blocked.
 		for(int i = 0; i < n; ++i) {
 			for(int j = 0; j < n; ++j) {
 				System.out.print(grid[i][j]);
@@ -26,19 +49,63 @@ public class Percolation {
 			}
 			System.out.print("\n");
 		}
+		System.out.println("----------------------------------");
+		//checking the name of the sites
+		for(int i = 0; i < n; ++i) {
+			for(int j = 0; j < n; ++j) {
+				System.out.print(site[i][j]);
+				System.out.print("\t");
+			}
+			System.out.print("\n");
+		}
+		System.out.println("----------------------------------");		
+	}
+	
+	public boolean isOpen(int row, int col) {
+		// is site (row, col) open?
+		
+		if(row > N || col > N || row < 1 || col < 1) {
+			throw new java.lang.IndexOutOfBoundsException();
+		}
+		
+		if( grid[row-1][col-1] == 1 ) {
+			return false;
+		}else {
+			return true;
+		}		
+	}
+
+	public    void open(int row, int col) {
+		// open site (row, col) if it is not open already
+		
+		if(row > N || col > N || row < 1 || col < 1) {
+			throw new java.lang.IndexOutOfBoundsException();
+		}
+		//local variable, r & c to point the array spot
+		int r = row -1;
+		int p;
+		int q;
+		int c = col -1;
+		grid[r][c] = 0;
+		//check if adjacent sites is open and do union
+		//left side
+		if(grid[r][c-1] == 0) {
+			p = site[r][c];
+			q = site[r][c-1];
+
+		}
 		
 	}
 	
-	/*public boolean isOpen(int row, int col) {
-		// is site (row, col) open?
-		return True;
-	}*/
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.print("hello");
-		System.out.print("\n");
-		Percolation perc = new Percolation(5);
+		System.out.print("Enter the size:");
+		//System.out.print("\n");
+		int ip = StdIn.readInt();
+		N = ip;
+		Percolation perc = new Percolation(N);
+
 	}
 
 }
