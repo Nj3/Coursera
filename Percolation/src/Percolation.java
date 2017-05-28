0eea99b96fb20e9618d2d434941741ad2acf72f2
Nj3 <natarajitachi@hotmail.com>
2017-05-28 21:22:33 +0530

    refactored

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -1,210 +1,126 @@
-
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 	
-
-	private static int N = 0;
-	private int[][] grid;
-	public int[][] site;
-	private WeightedQuickUnionUF uf = new WeightedQuickUnionUF(N*N);
-
+	//declare instance variables as private
+	private int N = 0;
+	private boolean[][] grid;
+	private static WeightedQuickUnionUF uf;
+	
 	public Percolation(int n) {
-		// create n-by-n grid, with all sites blocked
+		/* create n by n grid with all sites blocked */
 		
-		if( n <= 0 ) {
+		N = n;
+		uf = new WeightedQuickUnionUF(N*N);
+		
+		//Exception Handling
+		if( N <= 0 ) {
 			throw new java.lang.IllegalArgumentException();
 		}
 		
-		int[][] tmp = new int[n][n];
-
-		for(int i = 0; i < n; ++i) {
-			for(int j = 0; j < n; ++j) {
-				tmp[i][j] = 1;
-			}
+		//Blocked - false, open - true. Creating a 2d array and blocking all sites
+		boolean[][] tmp_grid = new boolean[n][n];
+		for(int i=0; i<N; ++i) {
+			for(int j=0; j<N; ++j) { tmp_grid[i][j] = false; }
 		}
 		
-		// create another n-by-n grid which tells the name of the site with numbers from 0 to N*N - 1
-		int[][] temp_site = new int[n][n];
-		int name = 0;
-		for(int i = 0; i<n; ++i) {
-			for(int j = 0; j<n; ++j) {
-				temp_site[i][j] = name++;
-			}
-		}
-		grid = tmp;
-		site = temp_site;
-	/*	
-		// ensuring all sites are blocked.
-		for(int i = 0; i < n; ++i) {
-			for(int j = 0; j < n; ++j) {
-				System.out.print(grid[i][j]);
-				System.out.print("\t");
-			}
-			System.out.print("\n");
-		}
-		System.out.println("----------------------------------");
-		//checking the name of the sites
-		for(int i = 0; i < n; ++i) {
-			for(int j = 0; j < n; ++j) {
-				System.out.print(site[i][j]);
-				System.out.print("\t");
-			}
-			System.out.print("\n");
-		}
-		System.out.println("----------------------------------"); */		
+		grid = tmp_grid;
 	}
-	/*
-	public void printit() {
-		// ensuring all sites are blocked.
-		for(int i = 0; i < N; ++i) {
-			for(int j = 0; j < N; ++j) {
-				System.out.print(grid[i][j]);
-				System.out.print("\t");
-			}
-			System.out.print("\n");
-		}
-		System.out.println("----------------------------------");		
-		
-	}
-	*/
 	
-	public boolean isFull(int row, int col) {
-		// is site (row, col) full?
+	public void open(int row, int col) {
+		/* open site(row,col) if it's not open already */
 		
+		//declare current_site		
+		int current_site = ((row-1)*N) + (col-1);
+		
+		//Exception Handling
 		if(row > N || col > N || row < 1 || col < 1) {
 			throw new java.lang.IndexOutOfBoundsException();
 		}
 		
-		// r, c are spatial co-ords for 2d array
-		int r = row - 1;
-		int c = col - 1;
-		
-		//curr_site holds the positional value in the  union array.
-		int curr_site = site[r][c];
-		
-		//iterate the first row and check if its connected. If yes, return true else false
-		for( int i=1; i<=N; ++i ) {
-			if(isOpen(1,i)) {
-				if(uf.connected(i-1, curr_site)) {
-					return true;
-				}
-			}
-			}
-		return false;
+		//is the site open?
+		if(!isOpen(row, col)) {
+			grid[row-1][col-1] = true;
 		}
+		
+		//check if adjacent sites exists, If yes, check if its open. If yes, perform union, else pass.
+		
+		//left side
+		if( col-2 >= 0 && grid[row-1][col-2] ) { uf.union(current_site, current_site-1); }
+		//right side
+		if( col+1 <= N && grid[row-1][col] ) { uf.union(current_site, current_site+1); }
+		//up side
+		if( row-2 >= 0 && grid[row-2][col-1] ) { uf.union(current_site, current_site-N); }
+		//down side
+		if( row+1 <= N && grid[row][col-1] ) { uf.union(current_site, current_site+N); }
+		
+	}
 	
 	public boolean isOpen(int row, int col) {
-		// is site (row, col) open?
+		//is site(row, col) open?
 		
+		//Exception Handling
 		if(row > N || col > N || row < 1 || col < 1) {
 			throw new java.lang.IndexOutOfBoundsException();
 		}
 		
-		if( grid[row-1][col-1] == 1 ) {
-			return false;
-		}else {
-			return true;
-		}		
+		//returns true if its opened, false if its blocked
+		return grid[row-1][col-1];
+	
 	}
-
-	public     int numberOfOpenSites() {
-		// number of open sites
+	
+	public boolean isFull(int row, int col) {
+		//is site (row, col) Full?
+		 
+		//Exception Handling
+		if(row > N || col > N || row < 1 || col < 1) {
+			throw new java.lang.IndexOutOfBoundsException(); 
+		}
+		
+		//declare current_site		
+		int current_site = ((row-1)*N) + (col-1);
+		
+		//check whether top row sites are open and then check if its connected
+		for(int i=1; i<=N; ++i) {
+			if(isOpen(1,i)) {
+				if(uf.connected(i-1, current_site)) { return true; }
+			}
+		}
+		return false;
+	}
+	
+	public int numberofOpenSites() {
+		//number of open sites
 		int count = 0;
 		
-		for(int i = 0; i<N; ++i) {
+		for(int i=0; i<N; ++i) {
 			for(int j=0; j<N; ++j) {
-				if(grid[i][j] == 0) {
-					++count;
-				}
+				if(grid[i][j]) { ++count; }
 			}
 		}
 		return count;
 	}
 	
-	
-	public    void open(int row, int col) {
-		// open site (row, col) if it is not open already
-		
-		if(row > N || col > N || row < 1 || col < 1) {
-			throw new java.lang.IndexOutOfBoundsException();
-		}
-		
-		//local variable, r & c to point the array spot
-		int r = row -1;
-		int c = col -1; 
-		grid[r][c] = 0;
-		
-		//check if adjacent sites is open and do union
-		
-		//System.out.println("perform union with adjacent sides and print yes/no if itsopen");
-		if( c-1 >= 0 && grid[r][c-1] == 0 ) { //left side
-			uf.union(site[r][c], site[r][c-1]);
-			//System.out.println(uf.connected(site[r][c], site[r][c-1]));
-		} 
-		if( c+1 <= N-1 && grid[r][c+1] == 0 ) { //right side
-			uf.union(site[r][c], site[r][c+1]);
-			//System.out.println(uf.connected(site[r][c], site[r][c+1]));
-		} 
-		if( r-1 >= 0 && grid[r-1][c] == 0 ) { //up 
-			uf.union(site[r][c], site[r-1][c]);
-			//System.out.println(uf.connected(site[r][c], site[r-1][c]));
-		}
-		if( r+1 <= N-1 && grid[r+1][c] == 0 ) { // down
-			uf.union(site[r][c], site[r+1][c]);
-			//System.out.println(uf.connected(site[r][c], site[r+1][c]));
-		}
-	}
-	
 	public boolean percolates() {
-		// does the system percolate?
+		//does the system percolates?
 		for(int i=1; i<=N; ++i) {
 			if(isOpen(N,i)) {
-				if(isFull(N,i)) {
-					return true;
-				}
+				if(isFull(N,i)) { return true;	}
 			}
 		}
 		return false;
 	}
 	
 	/*
-	private static double[] mymain(int[] args) throws Exception {
-		// TODO Auto-generated method stub
-		//System.out.print("Enter the size:");
-		//System.out.print("\n");
-		int ip = args[0];
-		int trails = args[1];
-		
-		//xt is the array which contains number of open sites when system percolates
-		double[] xt = new double[trails];
-		
-		N = ip;		
-		Percolation perc = new Percolation(N);
-		for(int trail=0; trail<trails; ++trail) {
-			while(true) {
-				if(perc.percolates()) {
-					//System.out.println(perc.numberOfOpenSites()/(double)(N*N));
-					xt[trail]= perc.numberOfOpenSites()/(double)(N*N);
-					//System.out.println(xt[trail]);
-					break;
-				}
-				
-				int rand_site = StdRandom.uniform(N*N);
-				for(int i=0; i<N; ++i) {
-					for(int j=0; j<N; ++j) {
-						if(site[i][j] == rand_site) {
-							perc.open(i+1, j+1);
-							//perc.printit();
-						}
-					}
-				}
-				
-			}
-		}
-		return xt;
-	}
-	*/
-
-	
+	public static void main(String[] args) {
+		//test client
+		int n = 3;
+		Percolation p1 = new Percolation(n);
+		//uf = new WeightedQuickUnionUF(n*n);
+		p1.open(1, 1);
+		p1.open(2, 1);
+		p1.open(2, 2);
+		p1.open(3, 2);
+		System.out.print(p1.percolates());
+	}*/
 }
--- Percolation/src/PercolationStats.java
+++ Percolation/src/PercolationStats.java
@@ -1,103 +1,70 @@
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-
-	private static double[] total_trails;
-	private static int n = 0;
-	private static int T = 0;
-	//private double xbar; //mean
-	//private double sd; //standard deviation
 	
-	public PercolationStats(int n, int trails) {
+	//instance variables
+	private static double[] total_trails;
+	private static int T = 0;
+	
+	public PercolationStats(int n, int trials) {
+		// perform trials independent experiments on an n-by-n grid
 		
-		// Throw exception if n <= 0 or trails <=0
-		if(n<=0 || trails<=0) {
+		// Throw exception if n <= 0 or trials <=0
+		if(n<=0 || trials<=0) {
 			throw new java.lang.IllegalArgumentException();
 		}
 		
-		try {
-			Percolation perc = new Percolation(n);
-			for(int trail=0; trail<trails; ++trail) {
-				while(true) {
-					if(perc.percolates()) {
-						//System.out.println(perc.numberOfOpenSites()/(double)(N*N));
-						total_trails[trail]= perc.numberOfOpenSites()/(double)(n*n);
-						//System.out.println(xt[trail]);
-						break;
+		//initialize percolation object to perform independent trails
+		for(int t=0; t<trials; ++t) {
+			Percolation p = new Percolation(n);
+			while(true) {
+				if(p.percolates()) {
+					total_trails[t] = p.numberofOpenSites()/(double)(n*n);
+					break;
+				}
+				//pick a random site to open
+				int rand_site = StdRandom.uniform(n*n);
+				for(int row=0; row<n; ++row) {
+					for(int col=0; col<n; ++col) {
+						if( ((row*n) + col) == rand_site) { p.open(row+1, col+1); }
 					}
-					
-					int rand_site = StdRandom.uniform(n*n);
-					for(int i=0; i<n; ++i) {
-						for(int j=0; j<n; ++j) {
-							if(perc.site[i][j] == rand_site) {
-								perc.open(i+1, j+1);
-								//perc.printit();
-							}
-						}
-					}
-					
 				}
 			}
-
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		//System.out.print(total_trails[1]);
+		}	
 	}
 	
 	public double mean() {
-	   // sample mean of percolation threshold
-		/*
-		double sum=0;
-		for(int i=0; i<T; ++i) {
-			sum += total_trails[i];
-		}
-		return sum/T;
-		*/
+		// sample mean of percolation threshold
 		return StdStats.mean(total_trails);
 	}
 	
 	public double stddev() {
-	   // sample standard deviation of percolation threshold
-		/*
-		double sum=0;
-		double xbar = mean();
-		for(int i=0; i<T; ++i) {
-			sum += ((total_trails[i] - xbar) * (total_trails[i] - xbar));
-		}
-		return Math.sqrt(sum/(T-1));
-		*/
+		// sample standard deviation of percolation threshold
 		return StdStats.stddev(total_trails);
 	}
 	
 	public double confidenceLo() {
-	   // low  endpoint of 95% confidence interval
+		// low  endpoint of 95% confidence interval
 		double xbar = mean();
-		double s = stddev();
-		return xbar - ((1.96*s)/Math.sqrt(T));
+		double sd = stddev();
+		return xbar - ((1.96*sd)/Math.sqrt(T));
 	}
 	
-	public double confidenceHi()  {
-	   // high endpoint of 95% confidence interval
+	public double confidenceHi() {
+		// high endpoint of 95% confidence interval
 		double xbar = mean();
-		double s = stddev();
-		return xbar + ((1.96*s)/Math.sqrt(T));
+		double sd = stddev();
+		return xbar + ((1.96*sd)/Math.sqrt(T));
 	}
 
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		//n = 200;
-		//T = 100;
-		n = Integer.parseInt(args[0]);
+	public static void main(String[] args) {
+		// test client (described below)
+		int n = Integer.parseInt(args[0]);
 		T = Integer.parseInt(args[1]);
 		PercolationStats pstats = new PercolationStats(n, T);
-		//System.out.println(n);
-		System.out.println("the mean = " + pstats.mean());
-		System.out.println("The standard deviation = " + pstats.stddev());
-		System.out.println("95% confidence interval = [" + pstats.confidenceLo() + "," + pstats.confidenceHi() + "]" );
+		System.out.println("mean                    = " + pstats.mean());
+		System.out.println("stddev                  = " + pstats.stddev());
+		System.out.println("95% confidence interval = [" + pstats.confidenceLo() + ", " + pstats.confidenceHi() + "]");
 	}
-
 }
