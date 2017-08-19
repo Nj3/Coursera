917773840651c12ddee5be8e6ca6ba917e26500d
Nj3 <natarajitachi@hotmail.com>
2017-08-19 22:05:34 +0530

    initial commit

---
+++ Pattern_recognition/src/Point.java
@@ -0,0 +1,127 @@
+
+import java.util.Arrays;
+import java.util.Comparator;
+import edu.princeton.cs.algs4.StdDraw;
+
+/**
+ * @author Nj3
+ * Implementation of Point 2D (x, y)
+ */
+public class Point implements Comparable<Point> {
+
+	private final int x;
+	private final int y;
+	/**
+	 * Initializes a new point.
+	 * @param x, y It is the 2D spatial coordinates of a plane.
+	 */
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	/**
+     * Draws this point to standard draw.
+     */
+    public void draw() {
+        StdDraw.point(x, y);
+    }
+
+    /**
+     * Draws the line segment between this point and the specified point
+     * to standard draw.
+     *
+     * @param that the other point
+     */
+    public void drawTo(Point that) {
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    /**
+     * Returns the slope between this point and the specified point.
+     * Formally, if the two points are (x0, y0) and (x1, y1), then the slope
+     * is (y1 - y0) / (x1 - x0). For completeness, the slope is defined to be
+     * +0.0 if the line segment connecting the two points is horizontal;
+     * Double.POSITIVE_INFINITY if the line segment is vertical;
+     * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
+     *
+     * @param  that the other point
+     * @return the slope between this point and the specified point
+     */
+    public double slopeTo(Point that) {
+        double slope = (double) (that.y -  this.y) / (that.x - this.x);
+        if ( slope == +0.0 || slope == -0.0) return +0.0;
+        else return slope;
+    }
+
+    /**
+     * Compares two points by y-coordinate, breaking ties by x-coordinate.
+     * Formally, the invoking point (x0, y0) is less than the argument point
+     * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
+     *
+     * @param  that the other point
+     * @return the value <tt>0</tt> if this point is equal to the argument
+     *         point (x0 = x1 and y0 = y1);
+     *         a negative integer if this point is less than the argument
+     *         point; and a positive integer if this point is greater than the
+     *         argument point
+     */
+    public int compareTo(Point that) {
+        if (this.y == that.y && this.x == that.x) return 0;
+        else if (this.y < that.y) return -1;
+        else if (this.y == that.y && this.x < that.x) return -1;
+        else return 1;
+    }
+
+    /**
+     * Compares two points by the slope they make with this point.
+     * The slope is defined as in the slopeTo() method.
+     *
+     * @return the Comparator that defines this ordering on points
+     */
+    public final Comparator<Point> slopeOrder = new slopeOrder();
+    
+    private class slopeOrder implements Comparator<Point> {
+    	
+    	public int compare(Point a, Point b) {
+    		double slope_of_a = slopeTo(a);
+    		double slope_of_b = slopeTo(b);
+    		if ( slope_of_a < slope_of_b ) return -1;
+    		else if (slope_of_a == slope_of_b ) return 0;
+    		else return +1;
+    	}
+    }
+
+
+    /**
+     * Returns a string representation of this point.
+     * This method is provide for debugging;
+     * your program should not rely on the format of the string representation.
+     *
+     * @return a string representation of this point
+     */
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+    
+	public static void main(String[] args) {
+		// TODO Unit testing
+		Point p1 = new Point(5,0);
+		Point p2 = new Point(5,5);
+		Point p3 = new Point(1,7);
+		Point[] p = new Point[] {p1,p2,p3};
+		System.out.println(p1.toString());
+		System.out.println(p2.toString());
+		System.out.println(p3.toString());
+		System.out.println(p1.slopeTo(p2));
+		System.out.println(p2.slopeTo(p3));
+		Arrays.sort(p, 0, 2, Point.slopeOrder);
+		/*
+		StdDraw.setPenRadius(0.05);
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.point(0.5, 0.5);
+		StdDraw.setPenColor(StdDraw.MAGENTA);
+		StdDraw.line(0.2, 0.2, 0.8, 0.2); */
+	}
+
+}
