06a6fb9f172bfdd2366406549abaeb0c61415dcb
Nj3 <natarajitachi@hotmail.com>
2017-09-27 08:06:31 +0530

    Completed Testing. Submitted. checkstyle throwed error saying not to use TreeMap

--- 8puzzle/src/Solver.java
+++ 8puzzle/src/Solver.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -23,18 +22,17 @@ public class Solver {
 	 * 2. twinGameTree  -> game tree formed after finding twin of initial board.
 	 * 3. searchNode    -> Node which will be expanded at every iteration till puzzle is solved.
 	 * 4. path          -> way the board moved by sliding the blocks inside the board.
-	 * 5. exploredNodes -> list of game boards which is already expanded.
-	 * 6. searchNodeTwin-> node which will be expanded at every iteration where the source is twin of initial game board.
+	 * 5. searchNodeTwin-> node which will be expanded at every iteration where the source is twin of initial game board.
 	 */
 	public Solver(Board initial) {
 		if ( initial == null ) throw new java.lang.IllegalArgumentException();
 		
 		// local variable declarations
+		int minNumOfMoves = 0;
 		PriorityOrder po = new PriorityOrder();
 		MinPQ<GameBoard> gameTree = new MinPQ<GameBoard>(po);
 		MinPQ<GameBoard> twinGameTree = new MinPQ<GameBoard>(po);
 		Board[] path;
-		List<Board> exploredNodes = new ArrayList<Board>();
 		GameBoard searchNode = new GameBoard(initial, null);
 		GameBoard searchNodeTwin = new GameBoard(initial.twin(), null);
 		gameTree.insert(searchNode);
@@ -52,6 +50,7 @@ public class Solver {
 		// Iterating through the tree to find the paths
 		while ( true ) {
 			
+			if ( gameTree.isEmpty() ) return;
 			searchNode = gameTree.delMin();
 			numOfMoves = searchNode.movesToReachCurrentBoardState;
 			
@@ -60,8 +59,8 @@ public class Solver {
 				isSolvable = true;
 				
 				GameBoard boardToBeAddedInPath = searchNode;
-				System.out.println(numOfMoves);
 				int i = numOfMoves;
+				minNumOfMoves = numOfMoves;
 				path = new Board[i+1];
 				while ( i >= 0 ) {
 					path[i] = boardToBeAddedInPath.currentGameboard;
@@ -70,38 +69,40 @@ public class Solver {
 				}
 				goalPaths.put(numOfMoves, path);
 				
-				System.out.println(goalPaths.toString());
 				if ( gameTree.isEmpty() ) return;
 				else continue;
 			}
 			
 			numOfMoves++;
-			if ( !exploredNodes.contains(searchNode.currentGameboard) ) {
+			// minNumOfMoves will only be set when its reached the goal state atleast once.
+			// so we have to expanding node to take place by keeping the below condition
+			// which indicates we haven't reached the goal state yet.
+			if ( minNumOfMoves == 0 || numOfMoves < minNumOfMoves ) {
 				for ( Board b : searchNode.currentGameboard.neighbors() ) {
-					if ( !exploredNodes.contains(b) ) gameTree.insert(new GameBoard(b, searchNode));
+					if ( searchNode.previousGameBoard == null ) gameTree.insert(new GameBoard(b, searchNode)); // initial condition
+					else if ( !searchNode.previousGameBoard.currentGameboard.equals(b) ) gameTree.insert(new GameBoard(b, searchNode));
 				}
 			}
-			exploredNodes.add(searchNode.currentGameboard);
 			
 			// solving the twin board. if its solvable then the initial board is unsolvable.
-			searchNodeTwin = twinGameTree.delMin();	
-			
-			if ( searchNodeTwin.currentGameboard.isGoal() ) {
-				isSolvable = false;
-				return;
-			}
-			
-			if ( !exploredNodes.contains(searchNodeTwin.currentGameboard) ) {
-				for ( Board b : searchNodeTwin.currentGameboard.neighbors() ) {
-					if ( !exploredNodes.contains(b) ) twinGameTree.insert(new GameBoard(b, searchNodeTwin));
+			// Only execute the below till actual board is not solvable
+			if ( !isSolvable ) {
+				searchNodeTwin = twinGameTree.delMin();	
+				
+				if ( searchNodeTwin.currentGameboard.isGoal() ) {
+					isSolvable = false;
+					return;
 				}
-			}
 			
-			exploredNodes.add(searchNodeTwin.currentGameboard);
-			if ( twinGameTree.isEmpty() ) return;
+				for ( Board b : searchNodeTwin.currentGameboard.neighbors() ) {
+					if ( searchNodeTwin.previousGameBoard == null ) twinGameTree.insert(new GameBoard(b, searchNodeTwin)); // initial condition
+					else if ( !searchNodeTwin.previousGameBoard.currentGameboard.equals(b) ) twinGameTree.insert(new GameBoard(b, searchNodeTwin));
+				}
+			
+				if ( twinGameTree.isEmpty() ) return;
+			}
 		}
 	}
-	
 	
 	/* A class to store the gameboard with moves + Actual Board class
 	 * This is done because API doesn't allow moves as instance variable in Board class.
